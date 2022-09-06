fd4cdc5662e6478b0e465b4d115f813009444e54
Kristhian David Segura <kristhian.segura@mail.escuelaing.edu.co>
2022-09-06 11:43:10 -0500

    FINALIZADO

---
+++ src/main/java/com/restApi/entities/User.java
@@ -0,0 +1,66 @@
+package com.restApi.entities;
+
+import com.restApi.dto.UserDto;
+
+import java.util.Date;
+
+public class User {
+
+    private String id;
+    private String name;
+    private String email;
+    private String lastName;
+    private Date createdAt;
+
+    public User(String id, String name, String email, String lastName, Date createdAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.lastName = lastName;
+        this.createdAt = createdAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public UserDto toDto(){
+        return new UserDto(id, name, email, lastName, createdAt.toString());
+    }
+}
